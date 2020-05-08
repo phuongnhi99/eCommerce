ff5d3c4a972e3bc92288088f36cfd6359993dc7d
Nhi <45851291+phuongnhi99@users.noreply.github.com>
2020-05-08 11:06:55 +0700

    Update Congigurations

--- eCommerce.Data/Configurations/TransactionConfiguration.cs
+++ eCommerce.Data/Configurations/TransactionConfiguration.cs
@@ -14,7 +14,6 @@ namespace eCommerce.Data.Configurations
             builder.ToTable("Transactions");
             builder.HasKey(x => x.Id);
             builder.Property(x => x.Id).UseIdentityColumn();
-            builder.HasOne(x => x.AppUser).WithMany(x => x.Transactions).HasForeignKey(x => x.UserId);
         }
     }
 }
