44af27373f32a9fc8cac1972b31a3397c574528c
Nhi <45851291+phuongnhi99@users.noreply.github.com>
2020-06-09 18:26:13 +0700

    Update login

--- eCommerce.AdminApp/Controllers/HomeController.cs
+++ eCommerce.AdminApp/Controllers/HomeController.cs
@@ -6,9 +6,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using eCommerce.AdminApp.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace eCommerce.AdminApp.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -20,6 +22,7 @@ namespace eCommerce.AdminApp.Controllers
 
         public IActionResult Index()
         {
+            var user = User.Identity.Name;
             return View();
         }
 
