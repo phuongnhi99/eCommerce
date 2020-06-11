9e0cd86864c9fe1d553fd3752f865a9014411430
Nhi <45851291+phuongnhi99@users.noreply.github.com>
2020-06-11 16:34:44 +0700

    Update admin

--- eCommerce.AdminApp/Controllers/AccountController.cs
+++ eCommerce.AdminApp/Controllers/AccountController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace eCommerce.AdminApp.Controllers
 {
-    public class CustomerController : Controller
+    public class AccountController : Controller
     {
         public IActionResult Index()
         {
